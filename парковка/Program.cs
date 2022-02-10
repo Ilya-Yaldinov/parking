dc3b3b06e79a4f8d1e5671d2830bc046f25d534a
Ilya-Yaldinov <90057250+Ilya-Yaldinov@users.noreply.github.com>
2022-02-10 21:54:08 +0500

    work

---
+++ парковка/Program.cs
@@ -0,0 +1,15 @@
+﻿class MyClass
+{
+    static void Main()
+    {
+        int time = Convert.ToInt32(Console.ReadLine());
+        if (time % 24 <= 5)
+        {
+            Console.WriteLine(Convert.ToDouble((time/24)*15 + (time%24)));
+        }
+        else
+        {
+            Console.WriteLine(Convert.ToDouble((time/24)*15 + 5 + ((time%24)-5)*0.5));
+        }
+    }
+}
