9e96022a901eb384ee0987d1a4a8c719b056678d
Ilya-Yaldinov <90057250+Ilya-Yaldinov@users.noreply.github.com>
2022-02-10 22:10:12 +0500

    better

--- парковка/Program.cs
+++ парковка/Program.cs
@@ -5,11 +5,9 @@
         int time = Convert.ToInt32(Console.ReadLine());
         if (time % 24 <= 5)
         {
-            Console.WriteLine(Convert.ToDouble((time/24)*15 + (time%24)));
+            Console.WriteLine(Convert.ToDouble((time / 24) * 15 + (time % 24)));
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
         }
-        else
-        {
-            Console.WriteLine(Convert.ToDouble((time/24)*15 + 5 + ((time%24)-5)*0.5));
-        }
+        Console.WriteLine(Convert.ToDouble((time / 24) * 15 + 5 + ((time % 24) - 5) * 0.5));
     }
 }
